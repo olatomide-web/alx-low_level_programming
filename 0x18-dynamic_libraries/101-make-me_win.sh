f53286c62aa5a72c3d9bdb06db14fad3219625c3
Aborisade olatomide Joshua <joshboris2020@gmail.com>
2022-06-16 22:27:17 +0100

    Update 101-make-me_win.sh

--- 0x18-dynamic_libraries/101-make-me_win.sh
+++ 0x18-dynamic_libraries/101-make-me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/olatomide-web/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libdynamic.so
+wget -P /tmp https://github.com/olatomide-web/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD=/tmp/libdynamic.so
