4b5fde883a73c85baa1b1353bad4c42cb6eaa4df
Aborisade olatomide Joshua <joshboris2020@gmail.com>
2022-06-16 22:24:10 +0100

    mic

--- 0x18-dynamic_libraries/101-make-me_win.sh
+++ 0x18-dynamic_libraries/101-make-me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/olatomide-web/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp https://github.com/olatomide-web/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=/tmp/libdynamic.so
